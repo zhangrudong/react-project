3d0b11075fe6673017a3a82e2f29c6709c8de1d7
zhangrudong <1137921970@qq.com>
2019-06-20 20:00:50 +0800

    创建项目-框架

---
+++ myapp/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '@/layout/App';
+import * as serviceWorker from './serviceWorker';
+import store from './store';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+import './main.scss'
+// import './mock/index'
+
+function renderFn () {
+  ReactDOM.render(
+    <HashRouter>
+      <Switch>
+        <Route path = "/" component = { App } />
+      </Switch>
+    </HashRouter>, document.getElementById('root'));
+}
+
+renderFn();
+store.subscribe(renderFn)
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ myapp/src/views/find/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Com extends Component {
+  componentDidMount () {
+  
+  }
+
+  render () {
+    return (
+      <div className = "box">
+        <header className = "header">发现头部</header>
+        <div className = "content">发现内容</div>
+      </div>
+    )
+  }
+}
+
+export default Com;
